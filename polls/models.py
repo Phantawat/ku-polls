eff137cde35d71dc56b47bba4a7a7860d7aa1fa6
Phantawat Lueangsiriwattana <phantawat.l@ku.th>
2024-08-29 15:46:21 +0700

    Add docstring in polls.models

--- polls/models.py
+++ polls/models.py
@@ -5,21 +5,37 @@ from django.utils import timezone
 
 
 class Question(models.Model):
+    """
+    A Question model class contains question attributes
+    and a method for checking is question published
+    """
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
+        """
+        Return a question string.
+        """
         return self.question_text
 
     def was_published_recently(self):
+        """
+        A method for checking is a question published.
+        """
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
+    """
+    A Choice model class contains choice attributes
+    """
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
+        """
+        Return a choice string.
+        """
         return self.choice_text
