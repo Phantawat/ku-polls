5554dc8bb37fb470f7f54f9d7de76acc15f5a62a
Phantawat Lueangsiriwattana <phantawat.l@ku.th>
2024-09-05 16:39:54 +0700

    create views.py and add signup method

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
+from . import views
 
 urlpatterns = [
     path('', RedirectView.as_view(url='polls', permanent=True)),
